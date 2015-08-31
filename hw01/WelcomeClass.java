a8f310e5039fee62cf13d1850222b80eca09841d
sls218 <sls218@lehigh.edu>
2015-08-31 00:09:17 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+///////////////////////
+//Steph Stieber
+//HW 1
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class 
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+//  print the design
+    System.out.println("   -----------");
+    System.out.println("   | WELCOME |");
+    System.out.println("   -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-S--L--S--2--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("I am on the varsity volleyball team");
+    
+        //test/testtest
+    }
+}
